a09fb05771ae9b8b06726fe6e10ca6a81e89a9c5
Alberto <agonzalez@tribalyte.com>
2021-02-03 00:11:52 +0100

    Implements likePost mutation
    - Implements like post toggle mutation, if the post is liked unlike and if the post is unliked then like

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -61,6 +61,29 @@ module.exports = {
             } catch(err) {
                 throw new Error(err);
             }
+        },
+        likePost: async (_, { postId }, context) => {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if (post) {
+                console.log(post.body);
+                // Check if the post already contains a like for current user
+                if (post.likes.find(like => like.username === username)) {
+                    // Post already liked, unlike it
+                    post.likes = post.likes.filter(like => like.username !== username);
+                } else {
+                    // post not liked, like it
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    });
+                }
+                await post.save();
+                return post;
+            } else {
+                throw new UserInputError("Post not found");
+            }
         }
     }
 };
