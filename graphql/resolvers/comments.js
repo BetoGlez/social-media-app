cab96b377ece9fb7f29da55758c8232e25ca09b5
Alberto <agonzalez@tribalyte.com>
2021-02-02 23:53:36 +0100

    Implements deleteComment mutation
    - Add the deleteComment mutation to GraphQL

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require("apollo-server");
+const { UserInputError, AuthenticationError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
@@ -24,6 +24,28 @@ module.exports = {
                 }
             } else {
                 throw new UserInputError("The comment body cannot be empty", { errors });
+            }
+        },
+        deleteComment: async (_, { postId, commentId }, context) => {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if (post) {
+                const commentIndex = post.comments.findIndex(comment => comment.id === commentId);
+                if (commentIndex > -1) {
+                    if (post.comments[commentIndex].username === username) {
+                        post.comments.splice(commentIndex, 1);
+                        await post.save();
+                        return post;
+                    } else {
+                        throw new AuthenticationError("Action not allowed");
+                    }
+                } else {
+                    throw new UserInputError("Comment not found");
+                }
+            } else {
+                throw new UserInputError("Post not found");
             }
         }
     }
