b7892c6801f2f31ef913a05c2f81a071e6c498a6
Alberto <agonzalez@tribalyte.com>
2021-02-05 01:19:31 +0100

    Persist user login data
    - Once the user is logged in persist the data and save it in case the user refreshed the page or come back within the token expiration time

--- social-media-client/src/data/AuthContextPrivider.tsx
+++ social-media-client/src/data/AuthContextPrivider.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import jwtDecode, { JwtPayload } from "jwt-decode";
+
 import AuthContext, { AuthContextModel } from "./auth-context";
 import { IUser } from "../graphql/models/user.model";
 
@@ -6,12 +8,40 @@ const AuthContextProvider: React.FC = (props) => {
 
     const [user, setUser] = useState<IUser | null>(null);
 
+    useEffect(() => {
+        // Persist data from local storage
+        const token = localStorage.getItem("jwtToken");
+        if(token) {
+            console.log("Token found in local storage");
+            const decodedToken = jwtDecode<JwtPayload>(token);
+            if (decodedToken.exp && (decodedToken.exp * 1000 < Date.now())) {
+                console.log("Token expired");
+                localStorage.removeItem("jwtToken");
+            } else {
+                const localStorageUser = decodedToken as IUser;
+                const composedUser: IUser = {
+                    id: localStorageUser.id,
+                    email: localStorageUser.email,
+                    username: localStorageUser.username,
+                    createdAt: "",
+                    token
+                };
+                console.log("Set current user to local storage user: ", composedUser);
+                setUser(composedUser);
+            }
+        } else {
+            console.log("No token found in local storage");
+        }
+    }, []);
+
     const login = (user: IUser) => {
         setUser(user);
+        localStorage.setItem("jwtToken", user.token);
     };
 
     const logout = () => {
         setUser(null);
+        localStorage.removeItem("jwtToken");
     };
 
     const authContext: AuthContextModel = {
